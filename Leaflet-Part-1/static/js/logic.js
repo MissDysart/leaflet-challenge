7590a8856a31ad84dd39f731ac140e86b743ba04
MissDysart <kelsy.dysart@gmail.com>
2024-04-01 17:13:38 -0600

    CSS updated, Part 1 complete

--- Leaflet-Part-1/static/js/logic.js
+++ Leaflet-Part-1/static/js/logic.js
@@ -19,16 +19,57 @@ d3.json(usgsData).then(function(data) {
 
 function createFeatures(earthquakeData) {
 
+    // Help from https://leafletjs.com/examples/choropleth/
+    function getColor(d) {
+        return d > 90 ? '#0c2c84' :
+               d > 70 ? '#225ea8' :
+               d > 50 ? '#1d91c0' :
+               d > 30 ? '#41b6c4' :
+               d > 10 ? '#7fcdbb' :
+               d > -10 ? '#c7e9b4' :
+                '#ffffcc' ;
+    }
+
     // Use onEachFeature function for each feature in an array.
     // Popup into for each feature includes earthquake magnitude, location and depth
     function onEachFeature(feature, layer) {
-        layer.bindPopup(`<h3>${feature.properties.place}</h3><hr>
-        <p>Magnitude: ${feature.properties.mag}<br>Depth: ${feature.geometry.coordinates[2]}</p>`);
+        layer.bindTooltip(`<strong>Located ${feature.properties.place}</strong><br>
+        Magnitude: ${feature.properties.mag}<br>Depth: ${feature.geometry.coordinates[2]}`);
+    }
+
+    // Change the markers to circles
+    function pointToLayer(feature, latlng) {
+        // Circle marker properties
+        let eqMarkerOption = {
+            radius: feature.properties.mag**2,
+            fillColor: getColor(feature.geometry.coordinates[2]),
+            fillOpacity: 1,
+            color: "#000",
+            weight: 1
+        };
+        return L.circleMarker(latlng, eqMarkerOption);
     }
 
     let earthquakes = L.geoJSON(earthquakeData, {
-        onEachFeature:onEachFeature
+        onEachFeature: onEachFeature,
+        pointToLayer: pointToLayer
     }).addTo(eqMap);
+
+    //Add a legend to the map
+    let legend = L.control({position: "bottomright"});
+    legend.onAdd = function() {
+        let div = L.DomUtil.create("div", "info legend"),
+            grades = [-10, 10, 30, 50, 70, 90],
+            labels = [];
+        for (let i=0; i<grades.length; i++) {
+            div.innerHTML +=
+                "<i style=\"background-color:" + getColor(grades[i] + 1) + "\"></i>" +
+                grades[i] + (grades[i+1] ? " &ndash; " + grades[i+1] + "<br>" : "+");
+        }
+        return div;
+    }
+    legend.addTo(eqMap);
+
 }
 
 // function createMap(earthquakes) {}
